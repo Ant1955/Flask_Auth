21d6fcd67d65458a84fe7a48620f1fa6af7d0943
Ant1955 <abb1955@gmail.com>
2024-07-31 16:32:30 +0300

    VD07 task final

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from app import app
 
+# with app.app.context(): # ѕосле первого запуска эту строку можно удалить
+#    db.create_all() # ѕосле первого запуска эту строку можно удалить
+
 if __name__ == "__main__":
     app.run(debug=True)
 
